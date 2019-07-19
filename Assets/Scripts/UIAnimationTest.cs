4d9d08fa3ca59034dcfdb12b5fa98e0f99f1ef2c
Sir Misha <sir.misha@theknightsofunity.com>
2019-07-19 17:54:04 +0200

    dotween

---
+++ Assets/Scripts/UIAnimationTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAnimationTest : MonoBehaviour
+{
+    public Image testImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            PunchImage();
+        }
+    }
+
+    private void PunchImage()
+    {
+        
+    }
+}
