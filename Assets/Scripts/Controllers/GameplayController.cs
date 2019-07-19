a7408e769bf3c0f94356edd2775e36b449aadc74
Anna Nowak <annadnowak98@gmail.com>
2019-07-20 01:11:20 +0200

    Dodanie bazowego gameplayu, szikelet playera

--- Assets/Scripts/Controllers/GameplayController.cs
+++ Assets/Scripts/Controllers/GameplayController.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Data;
 using UnityEngine;
 
@@ -8,14 +9,26 @@ namespace Controllers
     {
         public ButtonIconsController iconsController;
         public PlayerIconsController playerIconsController;
+        KeyPressedController keyPressedController;
+        List<string> CurrentRoundQueue;
+        List<string> ActionQueue;
+        List<Player> players;
+        Player currentPlayer;
+
+        public void Awake()
+        {
+            CurrentRoundQueue = new List<string>();
+            ActionQueue = new List<string>();
+            keyPressedController = gameObject.GetComponent<KeyPressedController>();
+            keyPressedController.ButtonPressedEvent += ButtonPressed;
+        }
 
         private IEnumerator Start()
         {
-            this.playerIconsController.SetPlayers(new []
-            {
-                new Player { color = Color.cyan },
-                new Player { color = Color.red },
-            });
+            players = new List<Player>()
+            {  new Player { color = Color.cyan },
+                new Player { color = Color.red }};
+            this.playerIconsController.SetPlayers(players.ToArray());
 
             while (true)
             {
@@ -23,6 +36,30 @@ namespace Controllers
 
                 this.playerIconsController.Next();
                 this.iconsController.SetFront(new Button { name = "x" });
+            }
+        }
+
+        public void ButtonPressed(string buttonChanged)
+        {
+            if(CurrentRoundQueue.Count == ActionQueue.Count)
+            {
+                ActionQueue.Add(buttonChanged);
+                CurrentRoundQueue.Clear();
+                Debug.Log($"dodano do kolejki {buttonChanged}, zmiana gracza");
+                return;
+            }
+            if(ActionQueue[CurrentRoundQueue.Count]==buttonChanged)
+            {
+                CurrentRoundQueue.Add(buttonChanged);
+                Debug.Log($"{buttonChanged}");
+                return;
+            }
+            else
+            {
+                Debug.Log("Gracz przegral");
+                Debug.Log("zmiana gracza");
+                ActionQueue.Clear();
+                CurrentRoundQueue.Clear();
             }
         }
     }
