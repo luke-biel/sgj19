91a8fe5c56d351d3201cbde12db2334d3b68a51f
Łukasz Biel <lukasz.biel@anixe.pl>
2019-07-19 18:15:18 +0200

    Add CarbonInput

---
+++ Assets/Scripts/Controllers/PlayUIController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Controllers
+{
+    public class PlayUIController : MonoBehaviour
+    {
+        public GameObject[] playerIcon;
+        public Transform sequenceGrid;
+        private GameObject[] sequence;
+        public Image pressedButton;
+    }
+}
